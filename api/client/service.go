51c1c835100318bbee91845d054619f84fd51af7
Paxx <daniel.botta@hiway.media>
2023-05-24 17:43:18 +0200

    feat: added resty client wrapper

---
+++ api/client/service.go
@@ -0,0 +1,74 @@
+package client
+
+import (
+	"encoding/json"
+	"github.com/go-resty/resty/v2"
+	"go.uber.org/zap"
+	"path"
+)
+
+type IService interface {
+	Get(route string, params map[string]string) (*resty.Response, error)
+	Delete(route string, params map[string]string) (*resty.Response, error)
+	Put(route string, body any, params map[string]string) (*resty.Response, error)
+	Post(route string, body any, params map[string]string) (*resty.Response, error)
+}
+
+type service struct {
+	client   *resty.Client
+	logger   *zap.SugaredLogger
+	endpoint string
+}
+
+func NewService(logger *zap.SugaredLogger, endpoint string) IService {
+	return &service{logger: logger, client: resty.New(), endpoint: endpoint}
+}
+
+func (s *service) send(method string, route string, params map[string]string, body any) (*resty.Response, error) {
+	request := s.client.R()
+	request.Method = method
+	request.URL = path.Join(s.endpoint, route)
+
+	for k, v := range params {
+		request.QueryParam.Add(k, v)
+	}
+
+	if body != nil {
+		j, err := json.Marshal(body)
+		if err != nil {
+			s.logger.Errorf("failed to serialize body")
+			return nil, err
+		}
+		request.Body = j
+		request.Header.Set("content-type", "application/json")
+	}
+
+	response, err := request.Send()
+
+	if err != nil {
+		s.logger.Errorf("issue while sending api: %v", err)
+		return nil, err
+	}
+
+	s.logger.Debugf("request: %s", request.Body)
+	s.logger.Infof("sent %s %s", request.Method, request.URL)
+	s.logger.Debugf("response: %s", string(response.Body()))
+
+	return response, nil
+}
+
+func (s *service) Get(route string, params map[string]string) (*resty.Response, error) {
+	return s.send("GET", route, params, nil)
+}
+
+func (s *service) Delete(route string, params map[string]string) (*resty.Response, error) {
+	return s.send("POST", route, params, nil)
+}
+
+func (s *service) Put(route string, body any, params map[string]string) (*resty.Response, error) {
+	return s.send("PUT", route, params, body)
+}
+
+func (s *service) Post(route string, body any, params map[string]string) (*resty.Response, error) {
+	return s.send("POST", route, params, body)
+}
--- api/go.mod
+++ api/go.mod
@@ -1,3 +1,14 @@
 module github.com/HiWay-Media/hwm-util/api
 
 go 1.18
+
+require (
+	github.com/go-resty/resty/v2 v2.7.0
+	go.uber.org/zap v1.24.0
+)
+
+require (
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/net v0.0.0-20211029224645-99673261e6eb // indirect
+)
