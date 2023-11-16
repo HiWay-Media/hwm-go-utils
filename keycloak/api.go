d0960635c4e0aca55a80775ff003fe38030713a7
Allan Nava <allannava95@gmail.com>
2023-11-16 10:43:08 +0100

    solve mispelling

--- keycloak/api.go
+++ keycloak/api.go
@@ -10,7 +10,7 @@ import (
 func (g *gkeycloak) GetToken( tokenOptions gocloak.TokenOptions ) (*JWT, error) {
 	token, err :=  g.GCloakClient.GetToken(g.ctx, g.realm, tokenOptions)
 	if err != nil {
-		return, nil, err
+		return nil, err
 	}
 	return token, nil
 }
