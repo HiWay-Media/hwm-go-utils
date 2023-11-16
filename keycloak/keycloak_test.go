ff72fdfe3fdefd84dbe434cf84f503c56203e8ec
Allan Nava <allannava95@gmail.com>
2023-11-16 14:12:00 +0100

    false

--- keycloak/keycloak_test.go
+++ keycloak/keycloak_test.go
@@ -23,7 +23,7 @@ func TestIKeycloak(t *testing.T) {
 	clientId := os.Getenv("KEYCLOAK_CLIENT_ID")
 	clientSecret := os.Getenv("KEYCLOAK_CLIENT_SECRET")
 	log.Println(realm, server, clientId, clientSecret)
-	k, err := NewKeycloak(context.Background(), realm, server, clientId, clientSecret, true)
+	k, err := NewKeycloak(context.Background(), realm, server, clientId, clientSecret, false)
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
