b81c3b445100d2769c6bed437847ff3d16705ce9
Allan Nava <allannava95@gmail.com>
2023-11-16 12:24:43 +0100

    added test

---
+++ keycloak/test_keycloak.go
@@ -0,0 +1,16 @@
+package test
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	if os.Getenv("APP_ENV") == "" {
+		err := os.Setenv("APP_ENV", "test")
+		if err != nil {
+			panic("could not set test env")
+		}
+	}
+	m.Run()
+}
