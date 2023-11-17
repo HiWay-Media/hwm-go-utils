cf58067cf1ecb9ff579c7eab317fb8c0732b1677
Allan Nava <allannava95@gmail.com>
2023-11-17 17:14:32 +0100

    feat: need to fix api_test.go

--- keycloak/api_test.go
+++ keycloak/api_test.go
@@ -1,25 +1,7 @@
 package keycloak_test
 
-import (
-	"context"
-	"os"
-	"testing"
 
-	"github.com/HiWay-Media/hwm-go-utils/keycloak"
-)
-
-func TestMain(m *testing.M) {
-	if os.Getenv("APP_ENV") == "" {
-		err := os.Setenv("APP_ENV", "test")
-		if err != nil {
-			panic("could not set test env")
-		}
-	}
-	
-	m.Run()
-}
-
-func TestIKeycloak(t *testing.T) {
+func TestAPI(t *testing.T) {
 	realm := os.Getenv("KEYCLOAK_REALM")
 	server := os.Getenv("KEYCLOAK_SERVER")
 	clientId := os.Getenv("KEYCLOAK_CLIENT_ID")
