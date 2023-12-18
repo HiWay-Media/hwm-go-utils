e6b09d61bc9b5c224a68901ee6af0e2c3425a39c
Allan Nava <allannava95@gmail.com>
2023-12-18 10:35:40 +0100

    added keydb

---
+++ keydb/keydb.go
@@ -0,0 +1,14 @@
+package keydb
+
+import (
+	"github.com/go-redis/redis/v8"
+)
+
+
+func GetKeyDBClient(addr string) *redis.ClusterClient {
+	return redis.NewClusterClient(&redis.Options{
+        Addr:     addr,
+        Password: "", // no password set
+        DB:       0,  // use default DB
+    })
+}
