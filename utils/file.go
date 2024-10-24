bfe903b4649d505dfd4f861d6b3c3c6d44bc3c77
Allan Nava <allannava95@gmail.com>
2024-10-24 20:11:24 +0200

    added string and file

---
+++ utils/file.go
@@ -0,0 +1,17 @@
+package utils
+
+import (
+	"os"
+    "log"
+)
+
+func WriteToFile(s []byte, file string) {
+	//
+	f, err := os.Create(file)
+	if err != nil {
+		//slack.Bug("WriteToFile", err.Error())
+		log.Fatal(err)
+	}
+	f.Write(s)
+	defer f.Close()
+}
--- utils/strings.go
+++ utils/strings.go
@@ -3,6 +3,8 @@ package utils
 import (
 	"regexp"
 	"strings"
+	"encoding/json"
+	"net/url"
 )
 
 /* 
@@ -29,4 +31,65 @@ func CleanUrlPath(url string) string {
 
 	// Recombine the protocol and cleaned path
 	return protocol + "://" + cleanedPath
+}
+
+func Contains(s []string, str string) bool {
+	for _, v := range s {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
+
+func ContainsWord(s, word string) bool {
+	// Convert both the string and the word to lowercase for case-insensitive comparison
+	s = strings.ToLower(s)
+	word = strings.ToLower(word)
+
+	// Check if the string contains the word
+	return strings.Contains(s, word)
+}
+
+func RemoveSlice(slice []string, s int) []string {
+	return append(slice[:s], slice[s+1:]...)
+}
+
+// print struct json format to console
+func PrettyPrint(i interface{}) string {
+	s, _ := json.MarshalIndent(i, "", "\t")
+	return string(s)
+}
+
+
+
+func EncodeURL(rawUrl string) string {
+	//return url.QueryEscape(s)
+	// Parse the URL
+	parsedUrl, err := url.Parse(rawUrl)
+	if err != nil {
+		fmt.Println("Error parsing URL:", err)
+		return ""
+	}
+
+	// Get the base URL (without query params)
+	baseUrl := parsedUrl.Scheme + "://" + parsedUrl.Host + parsedUrl.Path
+
+	// Get the query parameters and encode them
+	queryParams := parsedUrl.Query()
+	encodedParams := url.Values{}
+
+	// Loop through and encode each query parameter
+	for key, values := range queryParams {
+		for _, value := range values {
+			encodedParams.Add(key, url.QueryEscape(value))
+		}
+	}
+
+	// Combine the base URL with the encoded query parameters
+	finalUrl := baseUrl + "?" + encodedParams.Encode()
+
+	// Output the transformed URL
+	//fmt.Println("Encoded URL:", finalUrl)
+	return finalUrl
 }
