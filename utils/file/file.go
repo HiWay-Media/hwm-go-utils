42bd26302f3ac09bfa170cfeb893f4a69ef5ec2d
Allan Nava <allannava95@gmail.com>
2024-11-02 20:10:44 +0100

    added file exists fn

--- utils/file/file.go
+++ utils/file/file.go
@@ -9,9 +9,17 @@ func WriteToFile(s []byte, file string) {
 	//
 	f, err := os.Create(file)
 	if err != nil {
-		//slack.Bug("WriteToFile", err.Error())
 		log.Fatal(err)
 	}
 	f.Write(s)
 	defer f.Close()
+}
+
+// FileExists checks if a file exists and is not a directory.
+func FileExists(path string) bool {
+	info, err := os.Stat(path)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
